afde34ea5ab8fcab82cd4cc2dc6a3d70d4061e9c
Angela Zhang <azhang@plaid.com>
2018-04-09 10:20:41 -0700

    fix bug where we just return the 1st item for all users

--- server/src/resolvers/Query.js
+++ server/src/resolvers/Query.js
@@ -24,8 +24,12 @@ async function transactions(parent, { userId }, ctx, info) {
   const endDate = moment().format('YYYY-MM-DD');
 
   const userItems = await ctx.db.query.items({
-    user: { connect: { id: userId } }
+    where: { user: { id: userId } }
   });
+
+  if (!userItems || userItems.length === 0) {
+    return;
+  }
 
   // TODO: for now, assume each user only has 1 item.
   const accessToken = userItems[0].accessToken;
